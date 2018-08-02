ffaf8b54226cf1e3f62c247685aaf6fdc2986e34
Ricc-manc95 <riccard.manc@gmail.com>
2018-08-02 18:51:55 +0200

    menu a tendina ficco
    lol

--- D&DGridManager/app/src/main/java/com/unica/pregnantbrains/ddgridmanager/Grid.java
+++ D&DGridManager/app/src/main/java/com/unica/pregnantbrains/ddgridmanager/Grid.java
@@ -1,13 +1,37 @@
 package com.unica.pregnantbrains.ddgridmanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class Grid extends AppCompatActivity {
+
+    private DrawerLayout nDrawerLayout;
+    private ActionBarDrawerToggle nToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
+
+        nDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        nToggle = new ActionBarDrawerToggle(this, nDrawerLayout, R.string.open, R.string.close);
+
+        nDrawerLayout.addDrawerListener(nToggle);
+        nToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (nToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
