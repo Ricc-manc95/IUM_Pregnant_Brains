7a5e7707faadf8866a378d3e5cec0d027fe8ddf2
gsprega <simbhanad@gmail.com>
2018-08-27 18:44:16 +0200

    creation bar prova 1

--- D&DGridManager/app/src/main/java/com/unica/pregnantbrains/ddgridmanager/PawnCreation.java
+++ D&DGridManager/app/src/main/java/com/unica/pregnantbrains/ddgridmanager/PawnCreation.java
@@ -2,12 +2,19 @@ package com.unica.pregnantbrains.ddgridmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class PawnCreation extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pawn_creation);
+
+        /**Toolbar*/
+        mToolbar = (Toolbar) findViewById(R.id.creation_bar);
+        setSupportActionBar(mToolbar);
     }
 }
